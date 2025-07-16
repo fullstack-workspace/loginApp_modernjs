3b23a969725c1a8483b0dcb33aa7dde1ed13cce8
Black Cat <rborole06@gmail.com>
2025-07-16 14:19:05 +0530

    Create module-federation.config.ts

---
+++ module-federation.config.ts
@@ -0,0 +1,17 @@
+import { createModuleFederationConfig } from '@module-federation/modern-js'
+
+export default createModuleFederationConfig({
+    name: 'loginRemote',
+    manifest: {
+        filePath: 'static',
+    },
+    filename: 'static/remoteEntry.js',
+    exposes: {
+        // './Button': './src/components/Button.tsx',
+        './app': './src/export-App.tsx',
+    },
+    shared: {
+        react: { singleton: true },
+        'react-dom': { singleton: true },
+    },
+});
